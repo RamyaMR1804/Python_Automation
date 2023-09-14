fa3a2a12b148eb26da053c222bbe7d68e71cff48
RamyaMR1804 <120781835+RamyaMR1804@users.noreply.github.com>
2023-09-14 15:00:12 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -13,6 +13,6 @@ text me when you recive it.
 Sushant Singh
 """,'Email_attachment.txt']
 
-yag = yagmail.SMTP(user=sender,password="motykjhpexvdghmx")
+yag = yagmail.SMTP(user=sender,password="")
 yag.send(to=reciver,subject=subject,contents=contents)
 print("Email Sent")
